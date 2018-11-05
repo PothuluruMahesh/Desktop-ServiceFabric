03e49254a698044a023ad7c56ff35162c5f9eb2e
pmahesh6343@gmail.com <pmahesh6343@gmail.com>
2018-11-05 20:30:21 +0530

    DesktopStateless

---
+++ ServiceStateless/Web1/Controllers/GeoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : Controller
+    {
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Mahesh", "Pothuluru","HelloWorld" };
+        }
+    }
+}
